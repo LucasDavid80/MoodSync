459da0683779ff8147b0bf660c56e6cca4e4bcfc
Lucas David <lucasdavidoliveira80@gmail.com>
2025-03-31 20:53:17 -0300

    feat(sign_up): serviço de criação de usuário

--- lib/services/sing_up_service.dart
+++ lib/services/sing_up_service.dart
@@ -2,14 +2,30 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class SignUpService {
   FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-  registerUser({
+  Future<String?> registerUser({
     required String name,
     required String email,
     required String password,
   }) async {
-    UserCredential userCredential = await _firebaseAuth
-        .createUserWithEmailAndPassword(email: email, password: password);
+    try {
+      UserCredential userCredential = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
 
-    await userCredential.user!.updateDisplayName(name);
+      await userCredential.user!.updateDisplayName(name);
+
+      return null;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        return 'Esse email já está em uso.';
+      } else if (e.code == 'weak-password') {
+        return 'A senha deve ter pelo menos 6 caracteres.';
+      } else if (e.code == 'invalid-email') {
+        return 'Email inválido.';
+      } else if (e.code == 'operation-not-allowed') {
+        return 'Operação não permitida.';
+      } else {
+        return 'Erro desconhecido: ${e.message}';
+      }
+    }
   }
 }
