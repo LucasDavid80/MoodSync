bcd454696c9faa065cea587fedd28a674485977c
Lucas David <lucasdavidoliveira80@gmail.com>
2025-04-09 16:02:16 -0300

    fix: eliminando códigos

--- lib/models/my_events.dart
+++
@@ -1,9 +0,0 @@
-class MyEvents {
-  final String eventTitle;
-  final String eventDescp;
-
-  MyEvents({required this.eventTitle, required this.eventDescp});
-
-  @override
-  String toString() => eventTitle;
-}
--- test/mock_firebase.dart
+++
@@ -1,8 +0,0 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
-
-// Gerar mocks para FirebaseAuth e FirebaseApp
-@GenerateMocks([FirebaseAuth, FirebaseApp])
-void main() {}
